1a72e7c10544e17b771e5153d5dc4f145b01b8ea
Bjørn Vinther <g.bvinther@gmail.com>
2018-04-29 12:11:48 +0200

    portal first version

---
+++ Assets/Shared/Portal/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+	public float rotationSpeed;
+	public Transform anchor;
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.LookAt(anchor);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var dx = Input.GetAxis("Horizontal");
+		if (dx != 0)
+		{
+			transform.RotateAround(anchor.position, Vector3.up, -dx * rotationSpeed);
+		}
+	}
+}
---
+++ Assets/Shared/Portal/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+	public Transform portalCamera;
+	public Transform linkedPortal;
+	private float cameraDistance;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cameraDistance = portalCamera.localPosition.magnitude;
+		print(cameraDistance);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var dir = (Camera.main.transform.position - linkedPortal.position).normalized;
+		portalCamera.localPosition = dir * cameraDistance;
+		portalCamera.LookAt(transform);
+	}
+}
